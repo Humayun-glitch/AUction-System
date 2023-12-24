af18e7ba6c7bd3130a12cb9f02ce33db4090ad79
Humayun Saeed <57752996+Humayun-glitch@users.noreply.github.com>
2023-12-24 18:10:04 -0500

    Added server.h, client.h

---
+++ include/client.h
@@ -0,0 +1,9 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include "auction.h"
+
+// Function declaration for the client
+void start_auction_client(Auction *auction);
+
+#endif
---
+++ include/server.h
@@ -0,0 +1,9 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include "auction.h"
+
+// Function declaration for the server
+void start_auction_server(Auction *auction);
+
+#endif
