370cc20e2d069d2de186ffb7c48c181b3f69f2f4
Humayun Saeed <57752996+Humayun-glitch@users.noreply.github.com>
2023-12-24 06:10:46 -0500

    Update auction.h

--- include/auction.h
+++ include/auction.h
@@ -1,11 +1,86 @@
-// auction.h
 #ifndef AUCTION_H
 #define AUCTION_H
+
+// Define MAX_AUCTIONS here
+#define MAX_AUCTIONS 10
+
+#include <stdbool.h>  // Add this line to include the boolean type
 
 typedef struct {
     char item_name[256];  // Adjust the size as needed
     double starting_price;
     double current_highest_bid;
+    int id;
+    bool is_open;
+    // Add other parameters as needed
+} Auction;
+
+void initialize_auction(Auction* auction);
+void create_auction(Auction* auction, const char* item_name, double starting_price);
+void update_auction_status(Auction* auction);
+void manage_bidding_process(Auction* auction);
+void close_auction(Auction* auction);
+void bid_history(Auction* auction);
+
+#endif /* AUCTION_H */
+#ifndef AUCTION_H
+#define AUCTION_H
+
+#include <stdbool.h>  // Add this line to include the boolean type
+
+typedef struct {
+    char item_name[256];  // Adjust the size as needed
+    double starting_price;
+    double current_highest_bid;
+    int id;
+    bool is_open;
+    // Add other parameters as needed
+} Auction;
+
+void initialize_auction(Auction* auction);
+void create_auction(Auction* auction, const char* item_name, double starting_price);
+void update_auction_status(Auction* auction);
+void manage_bidding_process(Auction* auction);
+void close_auction(Auction* auction);
+void bid_history(Auction* auction);
+
+#endif /* AUCTION_H */
+#ifndef AUCTION_H
+#define AUCTION_H
+
+// Define MAX_AUCTIONS here
+#define MAX_AUCTIONS 10
+
+#include <stdbool.h>  // Add this line to include the boolean type
+
+typedef struct {
+    char item_name[256];  // Adjust the size as needed
+    double starting_price;
+    double current_highest_bid;
+    int id;
+    bool is_open;
+    // Add other parameters as needed
+} Auction;
+
+void initialize_auction(Auction* auction);
+void create_auction(Auction* auction, const char* item_name, double starting_price);
+void update_auction_status(Auction* auction);
+void manage_bidding_process(Auction* auction);
+void close_auction(Auction* auction);
+void bid_history(Auction* auction);
+
+#endif /* AUCTION_H */
+#ifndef AUCTION_H
+#define AUCTION_H
+
+#include <stdbool.h>  // Add this line to include the boolean type
+
+typedef struct {
+    char item_name[256];  // Adjust the size as needed
+    double starting_price;
+    double current_highest_bid;
+    int id;
+    bool is_open;
     // Add other parameters as needed
 } Auction;
 
