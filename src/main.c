c309200e1541b30d8354ff7e629f03cfcbcd0b04
Humayun Saeed <57752996+Humayun-glitch@users.noreply.github.com>
2023-12-24 06:08:07 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+// main.c
 #include <stdio.h>
 #include <stdlib.h>
 #include "auction.h"
@@ -7,21 +8,34 @@
 #include "realtime.h"
 #include "error.h"
 
+// Function declarations
+void manage_additional_options(Auction *auction);
+void create_auction_menu(Auction *auction);
+void update_auction_status_menu(Auction *auction);
+void manage_bidding_process_menu(Auction *auction);
+void close_auction_menu(Auction *auction);
+void bid_history_menu(Auction *auction);
+
 int main() {
     // Initialize any necessary components or structures
     Auction auction;
-    initialize_auction(&auction); // Implement initialization logic in auction.c
-
+    initialize_auction(&auction); // Implement initialization logic in 	     auction.c
+    // Declare the array of auctions
+    Auction auctions[2];
+    for (int i = 0; i < 2; ++i) {
+        initialize_auction(&auctions[i]);
+    }
+    
     // Prompt user for registration
     char username[50];
     char password[50];
-    
+    printf("Register \n");
     printf("Enter your username: ");
     scanf("%s", username);
-    
+
     printf("Enter your password: ");
     scanf("%s", password);
-    
+
     register_user(username, password);
 
     // Implement user authentication
@@ -38,33 +52,143 @@ int main() {
         scanf("%d", &choice);
 
         switch (choice) {
-            case 1:
-                browse_auctions();
-                break;
-            case 2:
-                place_bid();
-                break;
-            case 3:
-                initiate_auction();
-                break;
-            case 4:
-                monitor_bidding_history();
-                break;
-            case 5:
-                view_won_items();
-                break;
-            case 6:
-                exit_auction_system();
-                break;
-            default:
-                printf("Invalid choice. Please try again.\n");
+        case 1:
+            browse_auctions(auctions);
+            break;
+        case 2:
+            place_bid();
+            break;
+        case 3:
+            initiate_auction();
+            break;
+        case 4:
+            monitor_bidding_history();
+            break;
+        case 5:
+            view_won_items();
+            break;
+        case 6:
+            exit_auction_system();
+            break;
+        case 7:
+            printf("Creation Options");
+            manage_additional_options(&auction);
+            break;
+        default:
+            printf("Invalid choice. Please try again.\n");
         }
     } while (choice != 6);
 
-    
     // Cleanup and exit
     close_auction(&auction); // Implement cleanup logic in auction.c
     printf("Exiting the auction system.\n");
 
     return 0; // Indicates successful execution
+}
+
+void manage_additional_options(Auction *auction) {
+    // Implement additional options here
+    int option;
+    printf("Additional Options:\n");
+    printf("1. Create Auction\n");
+    printf("2. Update Auction Status\n");
+    printf("3. Manage Bidding Process\n");
+    printf("4. Close Auction\n");
+    printf("5. View Bid History\n");
+    printf("Choose an option (1-5): ");
+    scanf("%d", &option);
+
+    switch (option) {
+    case 1:
+        create_auction_menu(auction);
+        break;
+    case 2:
+        update_auction_status_menu(auction);
+        break;
+    case 3:
+        manage_bidding_process_menu(auction);
+        break;
+    case 4:
+        close_auction_menu(auction);
+        break;
+    case 5:
+        bid_history_menu(auction);
+        break;
+    default:
+        printf("Invalid option. Please try again.\n");
+    }
+}
+
+void create_auction_menu(Auction *auction) {
+    // Implement logic for creating an auction
+    char item_name[50];
+    double starting_price;
+
+    printf("Enter the name of the item: ");
+    scanf("%s", item_name);
+
+    printf("Enter the starting price: ");
+    scanf("%lf", &starting_price);
+
+    create_auction(auction, item_name, starting_price);
+}
+
+void update_auction_status_menu(Auction *auction) {
+    // Implement logic for updating auction status
+    if (auction == NULL) {
+        fprintf(stderr, "Invalid auction for updating status\n");
+        return;
+    }
+
+    // Perform any additional actions or validations if needed
+    // ...
+
+    // Call the function to update auction status
+    update_auction_status(auction);
+    printf("Auction status updated successfully!\n");
+}
+
+void manage_bidding_process_menu(Auction *auction) {
+    // Implement logic for managing bidding process
+    if (auction == NULL) {
+        fprintf(stderr, "Invalid auction for managing bidding process\n");
+        return;
+    }
+
+    // Perform any additional actions or validations if needed
+    // ...
+
+    // Call the function to manage bidding process
+    manage_bidding_process(auction);
+    printf("Bidding process managed successfully!\n");
+}
+
+void close_auction_menu(Auction *auction) {
+    // Implement logic for closing an auction
+    if (auction == NULL) {
+        fprintf(stderr, "Invalid auction for closing\n");
+        return;
+    }
+
+    // Perform any additional actions or validations if needed
+    // ...
+
+    // Call the function to close the auction
+    close_auction(auction);
+    printf("Auction closed successfully!\n");
+}
+
+void bid_history_menu(Auction *auction) {
+    // Implement logic for viewing bid history
+    if (auction == NULL) {
+        fprintf(stderr, "Invalid auction for viewing bid history\n");
+        return;
+    }
+
+    // Perform any additional actions or validations if needed
+    // ...
+
+    // Call the function to view bid history
+    bid_history(auction);
+    printf("Bid history viewed successfully!\n");
 }
